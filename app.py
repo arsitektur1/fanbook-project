f8904711b8a8a857846fdc3be95b7f8a4b56a969
arsitektur1 <bagaskur58@gmail.com>
2024-03-27 20:03:46 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://Bagas99:Kataksalto98@cluster0.m9sxgje.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -10,51 +21,22 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    
-    max_num = db.bucket.find_one(sort=[("num", -1)])
-    if max_num:
-        max_num= max_num['num']
-    else:
-        max_num = 0
-    num = max_num + 1
-    doc={
-        'num': num,
-        'bucket': bucket_receive,
-        # 0 artinya tugas belum selesai
-        # 1 artinya tugas sudah selesai
-        'done': 0
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': 'Data Saved!'})
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'Comment Posted!'})
 
-# done
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    # membuat tombol done nya aktif
-    num_receive = request.form['num_give']
-    db.bucket.update_one(
-        # mengapa menggunakn int? karena mencegah kalo user masukan tipe data "string".
-        {'num': int(num_receive)},
-        {'$set': {'done': 1}}
-    )
-    return jsonify({'msg': 'Update Done!'})
-
-# delete
-@app.route("/bucket/delete", methods=["POST"])
-def bucket_delete():
-    num_receive = request.form['num_give']
-    db.bucket.delete_one(
-        {'num': int(num_receive)}
-    )
-    return jsonify({'msg': 'Data Deleted!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': buckets_list})
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
